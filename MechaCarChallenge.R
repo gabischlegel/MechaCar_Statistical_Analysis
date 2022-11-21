e5d6cd9f007b06432f8e1443fe84db1c0a60f038
gabischlegel <110864175+gabischlegel@users.noreply.github.com>
2022-11-21 16:32:12 -0600

    Deliverable 1

---
+++ MechaCarChallenge.R
@@ -0,0 +1,5 @@
+library(dplyr)
+MechaCar_df <- read.csv('MechaCar_mpg.csv')
+head(MechaCar_df)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_df)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_df))
